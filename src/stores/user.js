0eb1c3cc389c7c7b4e0a86a9aa250460ee37498c
nazeerou <naziru2007@gmail.com>
2026-06-17 11:18:19 +0300

    fixed

--- src/stores/user.js
+++ src/stores/user.js
@@ -2,7 +2,7 @@ import { defineStore } from 'pinia'
 import axios from 'axios'
 
 // const API_URL = import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000/api/v1'
-const API_URL = import.meta.env.VITE_API_URL || 'https://web.bas.co.tz/api/v1'
+const API_URL = import.meta.env.VITE_API_URL || 'https://api.ebon.bas.co.tz/api/v1'
 
 export const useUserStore = defineStore('user', {
   state: () => ({
