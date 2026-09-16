59289a0e1b9e7d17748219ecaf48984324342903
nazeerou <naziru2007@gmail.com>
2026-09-16 10:38:52 +0300

    added

--- src/views/machines/BranchesList.vue
+++ src/views/machines/BranchesList.vue
@@ -319,7 +319,7 @@ import debounce from 'lodash/debounce'
 import axios from 'axios'
 
 // const API_URL = import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000/api/v1'
-const API_URL = import.meta.env.VITE_API_URL || 'https://ebon.bas.co.tz/api/v1'
+const API_URL = import.meta.env.VITE_API_URL || 'https://api.ebon.bas.co.tz/api/v1'
 
 // Branches list
 const branches = ref([])
